aa65c8cf9bad9221043fc465881baf4d7b8a90f0
souoGael <2494554@students.wits.ac.za>
2024-09-28 01:47:05 +0200

    test14

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => { res.send("Express Gael") } );
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
