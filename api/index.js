486c287ab097ca4cc0d4c6253e0b8c8d470f16b8
souoGael <2494554@students.wits.ac.za>
2024-09-28 02:24:53 +0200

    test15

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ admin.initializeApp({
   
 const db = admin.firestore();
 
-app.get("/", (req, res) => { res.send("Express Gael") } );
+app.get("/", (req, res) => { res.send("Express campus-transportation live") } );
 
 // API route to fetch data from Firestore
 app.get('/getSchedule', async (req, res) => {
@@ -28,7 +28,88 @@ app.get('/getSchedule', async (req, res) => {
     }
   });
 
+// API route to fetch data from Firestore about rent
+app.get('/getRent', async (req, res) => {
+    try {
+      const snapshot = await db.collection('Rental Station Inventory').get();
+  
+      let data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching data', error });
+    }
+  });
+  
+  // API route to fetch data from Firestore about main locations
+  app.get('/getLocations', async (req, res) => {
+    try {
+      const snapshot = await db.collection('Main Locations').get();
+  
+      let data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching data', error });
+    }
+});
+  
+// Rent
+app.post('/rent', async (req, res) => {
+    const { rentalId, userId, item, location } = req.body; // Get userId and location from the request body
 
+    try {
+
+        const decrement = admin.firestore.FieldValue.increment(-1);
+
+        // Reference to the rental document
+        const rentalRef = db.collection('Rental Station Inventory').doc(rentalId);
+        const rentalDoc = await rentalRef.get();
+
+        if (!rentalDoc.exists) {
+        return res.status(404).json({ message: 'Rental item not found' });
+        }
+
+        const rentalData = rentalDoc.data();
+
+        // Check availability
+        if (rentalData.availability <= 0) {
+        return res.status(400).json({ message: 'Item not available for rent' });
+        }
+
+        // Decrease availability
+        rentalRef.update({
+        availability: decrement
+        });
+
+        // Reference to the user document
+        const userRef = db.collection('Users').doc(userId);
+        
+        // Fetch the user document
+        const userDoc = await userRef.get();
+
+        // Check if the user document exists
+        if (!userDoc.exists) {
+        return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update the document by adding a new field
+        await userRef.update({
+        item: item,
+        location: location // Add or update the location field
+        });
+
+        return res.status(200).json({ message: 'Location added successfully' });
+    } catch (error) {
+        console.error('Error updating document: ', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+  
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
