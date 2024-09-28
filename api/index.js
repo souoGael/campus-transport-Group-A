6d7bc0460528fc672af6e13ad91ebcb70cad2fd4
souoGael <2494554@students.wits.ac.za>
2024-09-28 02:07:33 +0200

    test15

--- api/index.js
+++ api/index.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const app = express();
+const admin = require('firebase-admin');
+
+// Initialize the Firebase Admin SDK
+const serviceAccount = require('./serviceAccount.js');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  
+const db = admin.firestore();
 
 app.get("/", (req, res) => { res.send("Express Gael") } );
+
+// API route to fetch data from Firestore
+app.get('/getSchedule', async (req, res) => {
+    try {
+      const snapshot = await db.collection('Transportation Schedules').get();
+  
+      let data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching data', error });
+    }
+  });
+
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
