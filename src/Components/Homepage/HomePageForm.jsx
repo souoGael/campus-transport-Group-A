3700540b7995c1bd762c657d97ab35a107ebad6b
souoGael <2494554@students.wits.ac.za>
2024-10-01 00:02:01 +0200

    test34

--- src/Components/Homepage/HomePageForm.jsx
+++ src/Components/Homepage/HomePageForm.jsx
@@ -3,7 +3,7 @@ import "./HomePageForm.css";
 import SideMenu from "../SideMenu/SideMenu";
 import SearchBar from "../SearchBar/SearchBar";
 import { useLocation } from "react-router-dom";
-import BuildingMap from "../../BuildingMap.jsx";
+import BuildingMap from "../Map/BuildingMap.jsx";
 //import { FaRegListAlt } from "react-icons/fa";
 //import { IoEyeSharp, IoMailSharp } from "react-icons/io5";
 //import { useNavigate } from "react-router-dom";
